970ae0abee9d71361ec79b624f06dd6a01f65636
Lukas Dobbles <computorgenius101@gmail.com>
2022-04-12 20:45:46 -0700

    added clock for when game starts

--- client/src/components/MultiplayerHeader.jsx
+++ client/src/components/MultiplayerHeader.jsx
@@ -2,37 +2,57 @@ import { Text, View } from "react-native";
 import styles from "../styles";
 import { AntDesign } from "@expo/vector-icons";
 import { connect } from "react-redux";
+import { useEffect, useState } from "react";
 
-const MultiplayerHeader = ({ started }) => (
-	<View
-		style={{
-			flexDirection: "row",
-			flex: 1,
-			alignItems: "center",
-		}}
-	>
-		<Text style={styles.headerText}>Multiplayer</Text>
+const MultiplayerHeader = ({ started, time }) => {
+	const [delta, setDelta] = useState();
 
-		{started && (
-			<View
-				style={{
-					flexDirection: "row",
-					justifyContent: "flex-end",
-					flex: 1,
-				}}
-			>
-				<AntDesign
-					name="clockcircleo"
-					size={18}
-					style={{ position: "relative", top: 3, marginHorizontal: 2 }}
-					color="black"
-				/>
-				<Text style={styles.headerText}>0:00</Text>
-			</View>
-		)}
-	</View>
-);
+	useEffect(() => {
+		const id = setInterval(() => {
+			setDelta(new Date(new Date() - time));
+		}, 1000);
 
-const mapStateToProps = ({ multiplayer: { started } }) => ({ started });
+		return () => clearInterval(id);
+	}, [time]);
+
+	return (
+		<View
+			style={{
+				flexDirection: "row",
+				flex: 1,
+				alignItems: "center",
+			}}
+		>
+			<Text style={styles.headerText}>Multiplayer</Text>
+
+			{started && (
+				<View
+					style={{
+						flexDirection: "row",
+						justifyContent: "flex-end",
+						flex: 1,
+					}}
+				>
+					{delta && (
+						<Text style={styles.headerText}>
+							{delta.getMinutes()}:{delta.getSeconds()}
+						</Text>
+					)}
+					<AntDesign
+						name="clockcircleo"
+						size={18}
+						style={{ position: "relative", top: 3, marginHorizontal: 2 }}
+						color="black"
+					/>
+				</View>
+			)}
+		</View>
+	);
+};
+
+const mapStateToProps = ({ multiplayer: { started, time } }) => ({
+	started,
+	time,
+});
 
 export default connect(mapStateToProps)(MultiplayerHeader);
