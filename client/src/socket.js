a7e21064e76af229a97d774843d5c5e47094336c
Lukas Dobbles <computorgenius101@gmail.com>
2022-04-17 19:34:22 -0700

    used actual DEV variable

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,7 +1,5 @@
 import { io } from "socket.io-client";
 
 export default io(
-	process.env.__DEV__
-		? "ws://localhost:3000"
-		: "wss://tertle-server.herokuapp.com/"
+	__DEV__ ? "ws://localhost:3000" : "wss://tertle-server.herokuapp.com/"
 );
