cecaf7f55645f14bf46b6f559f6cfdfaf6bb23d4
Lukas Dobbles <computorgenius101@gmail.com>
2022-04-18 15:53:56 -0700

    removed push script from package.json that didn't work

--- client/src/components/MultiplayerHeader.jsx
+++ client/src/components/MultiplayerHeader.jsx
@@ -5,15 +5,17 @@ import { connect } from "react-redux";
 import { useEffect, useState } from "react";
 import * as util from "../util";
 
-const MultiplayerHeader = ({ started, time }) => {
+const MultiplayerHeader = ({ started, time, finished }) => {
 	const [delta, setDelta] = useState();
 
+	const adjustClock = () => {
+		console.log(finished);
+		if (finished) clearInterval(id);
+		else setDelta(new Date(new Date() - time));
+	};
+
 	useEffect(() => {
-		const id = setInterval(() => {
-			setDelta(
-				time ? new Date(new Date() - time) : new Date(new Date() - new Date())
-			);
-		}, 1000);
+		const id = setInterval(adjustClock, 1000);
 
 		return () => clearInterval(id);
 	}, [time]);
@@ -53,11 +55,12 @@ const MultiplayerHeader = ({ started, time }) => {
 
 const mapStateToProps = ({
 	multiplayer: {
-		multiplayer: { started, time },
+		multiplayer: { started, time, finished },
 	},
 }) => ({
 	started,
 	time,
+	finished,
 });
 
 export default connect(mapStateToProps)(MultiplayerHeader);
