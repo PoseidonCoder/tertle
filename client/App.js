f62c1e49cd93179383ad4fa913d1f9f2723fec17
Lukas Dobbles <computorgenius101@gmail.com>
2022-04-23 16:30:18 -0700

    added drawer navigation

--- client/App.js
+++ client/App.js
@@ -1,17 +1,14 @@
+import "react-native-gesture-handler";
 import Singleplayer from "./src/screens/Singleplayer";
 import SingleplayerHeader from "./src/components/SingleplayerHeader";
 import Multiplayer from "./src/screens/Multiplayer";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Home from "./src/screens/Home";
 import { Provider } from "react-redux";
 import store from "./src/store";
 import SettingsHeaderRight from "./src/components/SettingsHeaderRight";
-import styles from "./src/styles";
 import MultiplayerHeaderClock from "./src/components/MultiplayerHeaderClock";
-import { Text } from "react-native";
-
-const Stack = createNativeStackNavigator();
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import socket from "./src/socket";
 
 const config = {
 	screens: {
@@ -22,15 +19,19 @@ const config = {
 };
 
 const linking = {
-	prefixes: ["http://localhost:19006/", "tertle://"],
+	prefixes: ["http://localhost:19006/", "t ertle://"],
 	config,
 };
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
 	return (
 		<Provider store={store}>
 			<NavigationContainer linking={linking}>
-				<Stack.Navigator
+				<Drawer.Navigator
+					useLegacyImplementation
+					initialRouteName="Singleplayer"
 					screenOptions={{
 						headerTitleAlign: "center",
 						headerStyle: {
@@ -38,26 +39,18 @@ export default function App() {
 						},
 					}}
 				>
-					<Stack.Screen name="Home" component={Home} />
-					<Stack.Screen
-						name="Singleplayer"
-						component={Singleplayer}
-						options={() => ({
-							headerTitle: SingleplayerHeader,
-							headerRight: SettingsHeaderRight,
-						})}
-					/>
-					<Stack.Screen
+					<Drawer.Screen name="Singleplayer" component={Singleplayer} />
+					<Drawer.Screen
 						name="Multiplayer"
 						component={Multiplayer}
+						initialParams={{
+							id: socket.id,
+						}}
 						options={{
-							headerTitle: () => (
-								<Text style={styles.headerText}>Multiplayer</Text>
-							),
 							headerRight: () => <MultiplayerHeaderClock />,
 						}}
 					/>
-				</Stack.Navigator>
+				</Drawer.Navigator>
 			</NavigationContainer>
 		</Provider>
 	);
